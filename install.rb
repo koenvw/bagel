515cd9cdb5805e1d53f5b36b8ecef7dcfa37cfa3
kvw <kvw@024ce27e-8b1b-0410-8673-da5fc5b43585>
2007-04-24 07:32:54 +0000

    git-svn-id: http://svn.dotprojects.be/svn/bagel/trunk@2 024ce27e-8b1b-0410-8673-da5fc5b43585

--- install.rb
+++ install.rb
@@ -9,9 +9,46 @@ unless File.exists?(file)
   exit(1)
 end
 
+def insert_line(line,before,file)
+  # Tip from http://pleac.sourceforge.net/pleac_ruby/fileaccess.html
+  # 'Modifying a File in Place Without a Temporary File'
+  output= ""
+  inserted = false
+  File.open(file, 'r+') do |f|   # open file for update
+    # read into array of lines and iterate through lines
+    f.readlines.each do |line|
+      puts line
+      unless inserted
+        if line.gsub(/#.*/, '').include?(line)
+          inserted = true
+        elsif line.gsub(/#.*/, '').include?('Rails::Initializer.run')
+          output << line
+          output << "\n"
+          inserted = true
+        end
+      end
+      output << line
+    end
+    f.pos = 0                     # back to start
+    f.print output                # write out modified lines
+    f.truncate(f.pos)             # truncate to new length
+  end
+
+  unless inserted
+      STDERR.puts <<END
+ERROR: Could not update config/environment.rb
+To finish installation try to add the following line to
+config/environment.rb manually:
+\t#{LINE_TO_INSERT}
+NOTE: line must be inserted before Rails::Initializer.run()
+END
+      exit(1)
+  end
+end
+
 # add requires
-["require 'plugins/app_config/lib/configuration'","require 'memcache_util'","require 'mime/types'"].each |line|
-  insert_line line, 'Rails::Initializer.run', file
+["require 'plugins/app_config/lib/configuration'","require 'memcache_util'","require 'mime/types'"].each do |line|
+  insert_line(line, 'Rails::Initializer.run', file)
 end
 
 
@@ -31,7 +68,7 @@ config = <<EOC
   # email validation
   config.app_config.email_expression = /^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$/i
 EOC
-insert_line config, 'See Rails::Configuration for more options', file
+insert_line(config, 'See Rails::Configuration for more options', file)
 
 # add bagel_application_setup
 file = File.join(File.dirname(__FILE__), '../../../app/controllers/application.rb')
@@ -40,42 +77,5 @@ unless File.exists?(file)
   exit(1)
 end
 
-insert_line "bagel_application_setup", 'end', file
+insert_line("bagel_application_setup", 'end', file)
 
-def insert_line(line,before,file)
-  # Tip from http://pleac.sourceforge.net/pleac_ruby/fileaccess.html
-  # 'Modifying a File in Place Without a Temporary File'
-  output= ""
-  inserted = false
-  LINE_TO_INSERT = line
-  File.open(file, 'r+') do |f|   # open file for update
-    # read into array of lines and iterate through lines
-    f.readlines.each do |line|
-      puts line
-      unless inserted
-        if line.gsub(/#.*/, '').include?(LINE_TO_INSERT)
-          inserted = true
-        elsif line.gsub(/#.*/, '').include?('Rails::Initializer.run')
-          output << LINE_TO_INSERT
-          output << "\n"
-          inserted = true
-        end
-      end
-      output << line
-    end
-    f.pos = 0                     # back to start
-    f.print output                # write out modified lines
-    f.truncate(f.pos)             # truncate to new length
-  end
-
-  unless inserted
-      STDERR.puts <<END
-  ERROR: Could not update config/environment.rb
-  To finish installation try to add the following line to
-  config/environment.rb manually:
-  \t#{LINE_TO_INSERT}
-  NOTE: line must be inserted before Rails::Initializer.run()
-  END
-      exit(1)
-  end
-end
