a029d48c7caa0eda947f1e2df128dae43c5c9548
kvw <kvw@024ce27e-8b1b-0410-8673-da5fc5b43585>
2007-07-02 16:14:38 +0000

    - prepare_sitems: don't assume we have a ctype
    git-svn-id: http://svn.dotprojects.be/svn/bagel/trunk@71 024ce27e-8b1b-0410-8673-da5fc5b43585

--- lib/acts_as_content_type.rb
+++ lib/acts_as_content_type.rb
@@ -200,7 +200,7 @@ module ActsAsContentType
         wsitems = sitems.select {|v| v.website_id == new_sitem[:website_id].to_i }
         wsitem = wsitems.first
         # if we are using workflow only admins can change the status
-        if ctype.workflow
+        if ctype && ctype.workflow
           if AdminUser.current_user.is_admin?
             # workaround for checkboxes that do not return value when not set
             new_sitem[:status] = "0" unless new_sitem.has_key?(:status)
