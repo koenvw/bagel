59cd17548f57d3bdd14126b53675838fa94adc62
kvw <kvw@024ce27e-8b1b-0410-8673-da5fc5b43585>
2007-07-09 11:09:35 +0000

    - fixed login loop of death (don't redirect to a page where a user does not have access to, after login)
    git-svn-id: http://svn.dotprojects.be/svn/bagel/trunk@84 024ce27e-8b1b-0410-8673-da5fc5b43585

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -82,7 +82,7 @@ class Admin::HomeController < ApplicationController
   end
 
   private
-  
+
   def handle_login
     # Require username and password
     if params[:admin_user][:username].blank? or params[:admin_user][:password].blank?
@@ -103,10 +103,14 @@ class Admin::HomeController < ApplicationController
     session[:admin_user] = user.id
 
     # Redirect to requested page
-    requested_page = session[:requested_page] || { :controller => '/admin' }
+    if user.has_admin_permission?(:_main_menu_content)
+        requested_page = session[:requested_page] || { :controller => '/admin' }
+    else
+      requested_page = { :controller => '/admin', :action => 'me' }
+    end
     redirect_to requested_page
   end
-  
+
   def handle_login_reset_password
     # Require username
     if params[:admin_user][:username].blank?
