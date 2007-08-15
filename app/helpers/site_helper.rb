24a8bc4446af63f3e5eb37055bd7ef7077d77394
kvw <kvw@024ce27e-8b1b-0410-8673-da5fc5b43585>
2007-08-15 11:47:59 +0000

    - relations/_popup: show all relations if menu (menu is not really a content type) - fixed find_menu and added find_breadcrumb in site_helper
    git-svn-id: http://svn.dotprojects.be/svn/bagel/trunk@254 024ce27e-8b1b-0410-8673-da5fc5b43585

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -31,65 +31,6 @@ module SiteHelper
 
   end
 
-  def find_menu(site,menu_id)
-    root = Menu.find(menu_id)
-    items_not_published = Menu.find(:all,:conditions=>"
-        menus.parent_id='#{menu_id}'
-        AND sitems.website_id=#{Website.find_by_name(site).id}
-        AND sitems.is_published=0
-      ",
-      :include=>[:sitems]
-    )
-    root.all_children(:exclude => items_not_published)
-  end
-  
-  # Get menu as a tree hash (recursive method :/ )
-  def get_menu_tree(site, menu_id, linkTo = nil)
-    linkStr = ( linkTo.nil? ? site : linkTo )
-    menu_items = find_menu(site, menu_id)
-    tempArray = []
-    menu_items.each do |mitem|
-      if mitem.parent_id == menu_id
-        tempArray << get_menu_tree_hash(mitem, linkStr)
-      end
-    end
-    return tempArray
-  end
-  
-  # -- get menu children
-  private
-  def get_menu_tree_childs(parent, link)
-    if parent.has_children?
-      tempArray = []
-      parent.children.each do |child|
-        tempArray << get_menu_tree_hash(child, link)
-      end
-      return tempArray
-    end
-    return false
-  end
-
-  # -- hash structure
-  def get_menu_tree_hash(item, link)  
-    # if item is selected (based on uri parsing)? 
-    selected = false
-    request.request_uri.split("/").each do |uitem| 
-      if !uitem.blank? && !item.link.match("(http|ftp|https|)://.*")
-        item.link.split("/").each { |litem| selected = (uitem == litem ? true : false) }
-      end
-    end
-    #make the hash
-    myHash = {
-      :title => item.title,
-      :link => ( item.link.blank? ? "#" : item.link ),
-      :selected => selected,
-      :id => item.id,
-      :children => get_menu_tree_childs(item, link)
-    }
-    return myHash
-  end
-
-
   def truncate_with_words(text, length = 30, truncate_string = "...")
     if text.nil? then return end
 
@@ -98,5 +39,113 @@ module SiteHelper
 
     text.chars.length > length ? text.chars[0...nearest_left_space] + truncate_string : text
   end
+
+  # selected_menu_id is the id of currently selected menu
+  def find_menu(selected_menu_id)
+    if selected_menu_id
+      @menuselect = Menu.find(selected_menu_id.to_i)
+      selectpath = @menuselect.self_and_ancestors
+    else
+      @menuselect = nil
+      selectpath = []
+    end
+    parents = (selectpath.map{|m| m.parent}+[@menuselect]).uniq-[nil]
+    parents_sql_filter = parents.empty? ? '' : " OR parent_id IN (#{parents.map{|p| p.id}.join(',')})"
   
+    # retrieve only the menus that need to be open (and are published on current site)
+    allmenus = Menu.find(:all, 
+                         :conditions => "(parent_id IS NULL #{parents_sql_filter}) AND sitems.website_id=#{Website.find_by_name(site).id} AND sitems.is_published=1", 
+                         :order => 'lft', :include => :sitems)
+
+    # mark where to open and close html lists
+    @menus = []
+    allmenus.each_index do |i|
+        @menus[i] = {:indent => allmenus[i].level, 
+                     :title  => "#{allmenus[i].title}",
+                     :children_count => allmenus[i].all_children_count,
+                     :link  =>  allmenus[i].link.blank? ? "#" : allmenus[i].link}
+        @menus[i][:selected] =  allmenus[i] == @menuselect
+    end
+    allmenus[1..-1].each_index do |i|
+      # "open" means that we our menu item has children.
+      # "close" means that our items is the last one in the list (scope: indent)
+      @menus[i][:open] = @menus[i+1][:indent] > @menus[i][:indent]
+      @menus[i][:close] = [0, @menus[i][:indent] - @menus[i+1][:indent]].max
+    end
+    @menus.first[:open] = true
+    @menus.last[:open] = false
+    @menus.first[:close] = 0
+    @menus.last[:close] = 1
+    
+    return @menus
+  end
+
+ 
+  #
+  def find_breadcrumb(item, options={})
+    return if item.nil?
+    raise 'Not a nested set model !' unless item.respond_to?(:acts_as_nested_set_options)
+
+    @elements = []
+    item.self_and_ancestors.each do |ancestor|
+      @elements << {:title => ancestor.title,
+                    :link => ancestor.link,
+                    :close => 0}
+    end
+    @elements.last[:close] = 1
+    return @elements
+
+  end 
+
+  ### DEPRECATED
+
+    # Get menu as a tree hash (recursive method :/ )
+    def get_menu_tree(site, menu_id, linkTo = nil)
+      $stderr.puts('DEPRECATION WARNING: get_menu_tree is deprecated; please use find_menu')
+      linkStr = ( linkTo.nil? ? site : linkTo )
+      menu_items = find_menu(site, menu_id)
+      tempArray = []
+      menu_items.each do |mitem|
+        if mitem.parent_id == menu_id
+          tempArray << get_menu_tree_hash(mitem, linkStr)
+        end
+      end
+      return tempArray
+    end
+    
+    # -- get menu children
+    private
+    def get_menu_tree_childs(parent, link)
+      if parent.has_children?
+        tempArray = []
+        parent.children.each do |child|
+          tempArray << get_menu_tree_hash(child, link)
+        end
+        return tempArray
+      end
+      return false
+    end
+
+    # -- hash structure
+    def get_menu_tree_hash(item, link)  
+      # if item is selected (based on uri parsing)? 
+      selected = false
+      request.request_uri.split("/").each do |uitem| 
+        if !uitem.blank? && !item.link.match("(http|ftp|https|)://.*")
+          item.link.split("/").each { |litem| selected = (uitem == litem ? true : false) }
+        end
+      end
+      #make the hash
+      myHash = {
+        :title => item.title,
+        :link => ( item.link.blank? ? "#" : item.link ),
+        :selected => selected,
+        :id => item.id,
+        :children => get_menu_tree_childs(item, link)
+      }
+      return myHash
+    end
+
+
+
 end
