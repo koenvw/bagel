0090310140023ceeb50dd0ce41472dc537ad70db
kvw <kvw@024ce27e-8b1b-0410-8673-da5fc5b43585>
2007-08-24 12:18:51 +0000

    - document.rb: storage: database broken, use file_system for now
    git-svn-id: http://svn.dotprojects.be/svn/bagel/trunk@266 024ce27e-8b1b-0410-8673-da5fc5b43585

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,7 +1,9 @@
 class Document < MediaItem
   acts_as_content_type
-  has_attachment :storage       => :db_file,
-                 :content_type  => ["application/msword","application/pdf"]
+  has_attachment :storage       => :file_system,
+                 :path_prefix     => 'public/assets/documents', 
+                 #:content_type  => ["application/msword","application/pdf"],
+                 :size            => 0..15.megabyte # FIXME: yes we allow 0, sometimes the size is 0 after succesfull upload? 
   validates_as_attachment
   validates_presence_of :title
 
@@ -15,33 +17,33 @@ class Document < MediaItem
     record.extract_text
   end
 
-  def full_filename(thumbnail=nil)
-    'file://' + public_filename(thumbnail)
-  end
+  #def full_filename(thumbnail=nil)
+  #  'file://' + public_filename(thumbnail)
+  #end
 
-  def public_filename(thumbnail=nil)
-    '/media_item_from_db?id=' + id.to_s
-  end
+  #def public_filename(thumbnail=nil)
+  #  '/media_item_from_db?id=' + id.to_s
+  #end
 
   def extract_text
     # Create temporary working directory directory
     FileUtils.mkdir_p(File.join(RAILS_ROOT, 'tmp', 'documents'))
 
     # Find path to convert script
-    convert_script = File.join(RAILS_ROOT, 'script', FILTERS[record.content_type])
+    convert_script = File.join(RAILS_ROOT, 'script', FILTERS[self.content_type])
 
     # Get paths to temp in/out file
-    temp_file_in = record.create_temp_file.path
-    temp_file_out = File.join(RAILS_ROOT, 'tmp', 'documents',"#{record.id}.txt")
+    temp_file_in = self.create_temp_file.path
+    temp_file_out = File.join(RAILS_ROOT, 'tmp', 'documents',"#{self.id}.txt")
 
     # Convert
     if system "#{convert_script} #{temp_file_in} #{temp_file_out}"
-      record.content = File.read(temp_file_out)
+      self.content = File.read(temp_file_out)
       File.unlink(temp_file_out)
     else
-      record.content = "NO CONTENT"
+      self.content = "NO CONTENT"
     end
-    record.save
+    self.save
   end
 
 end
