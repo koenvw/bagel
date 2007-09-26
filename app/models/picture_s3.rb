2d88b48aea1a341389b29dda70b3202ee311d25c
js <js@024ce27e-8b1b-0410-8673-da5fc5b43585>
2007-09-26 08:59:40 +0000

    * fix syntax error (collection was missing a comma)
    git-svn-id: http://svn.dotprojects.be/svn/bagel/trunk@346 024ce27e-8b1b-0410-8673-da5fc5b43585

--- app/models/picture_s3.rb
+++ app/models/picture_s3.rb
@@ -4,7 +4,7 @@ class PictureS3 < MediaItem
 #                 :thumbnail_class => 'MediaItemThumbnail',
                  :thumbnails      => Setting.image_thumbnails,
                  :content_type    => :image, # not related to Bagel content types
-                 :processor       => Setting.image_processor
+                 :processor       => Setting.image_processor,
                  :size            => 0..10.megabyte, # FIXME: yes we allow 0, sometimes the size is 0 after succesfull upload?
                  :content_type   => ['image/jpeg', 'image/pjpeg', 'image/gif', 'image/png', 'image/x-png','image/bmp'] 
 
