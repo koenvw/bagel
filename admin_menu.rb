35cb8ff3bdf517d20b8ee07d09bb0faba1a6a8af
kvw <kvw@024ce27e-8b1b-0410-8673-da5fc5b43585>
2007-10-05 09:05:27 +0000

    - no they don't
    git-svn-id: http://svn.dotprojects.be/svn/bagel/trunk@368 024ce27e-8b1b-0410-8673-da5fc5b43585

--- admin_menu.rb
+++ admin_menu.rb
@@ -1,4 +1,3 @@
-require 'YAML'
 def admin_menu
   admin_menu_project_filename = File.join(RAILS_ROOT, 'config', 'admin_menu.yml')
   admin_menu_bagel_filename   = File.join(File.dirname(__FILE__), 'admin_menu.yml')
