bf38f9e83f422641ff8b2ee43ddb86213ecf3557
kvw <kvw@024ce27e-8b1b-0410-8673-da5fc5b43585>
2008-03-10 08:36:47 +0000

    - fixes for migrations for comments
    git-svn-id: http://svn.dotprojects.be/svn/bagel/trunk@456 024ce27e-8b1b-0410-8673-da5fc5b43585

--- db/migrate/026_create_comments.rb
+++ db/migrate/026_create_comments.rb
@@ -1,26 +1,24 @@
 class CreateComments < ActiveRecord::Migration
-  
 
-  
   def self.up
     # Create comments table
     create_table :comments do |t|
       t.column :body,          :text
       t.column :admin_user_id, :integer
-      t.column :updated_at,    :datetime
-      t.column :created_at,    :datetime
+      t.column :updated_on,    :datetime
+      t.column :created_on,    :datetime
     end
-    
+
     #Create sobjects_comments table
     create_table :sobjects_comments, :id => false do |t|
       t.column :sobject_id,    :integer
       t.column :comment_id,    :integer
     end
-    
+
   end
 
   def self.down
       drop_table :comments
-      drop_table :sobject_comments
+      drop_table :sobjects_comments
   end
 end
--- db/migrate/027_add_hide_comments_to_content_types.rb
+++ db/migrate/027_add_hide_comments_to_content_types.rb
@@ -1,12 +1,11 @@
 class AddHideCommentsToContentTypes < ActiveRecord::Migration
-  
+
   def self.up
-    add_column :content_types, :hide_comment, :boolean
+    add_column :content_types, :hide_comments, :boolean, :default => true
   end
-  
+
   def self.down
-    remove_column :content_types, :hide_comment
+    remove_column :content_types, :hide_comments
   end
-  
-  
+
 end
