599a8e4425dc98a7553da478901704b2a78291ed
kvw <kvw@024ce27e-8b1b-0410-8673-da5fc5b43585>
2008-04-30 14:22:49 +0000

    - fixed column type in add_hideimageupload_to_content_types
    git-svn-id: http://svn.dotprojects.be/svn/bagel/trunk@478 024ce27e-8b1b-0410-8673-da5fc5b43585

--- db/migrate/028_add_hideimageupload_to_content_types.rb
+++ db/migrate/028_add_hideimageupload_to_content_types.rb
@@ -1,8 +1,9 @@
 class AddHideimageuploadToContentTypes < ActiveRecord::Migration
   def self.up
-    add_column :content_types, :hide_imageuploads, :tinyint
+    add_column :content_types, :hide_imageuploads, :boolean, :default => true
   end
 
   def self.down
+    remove_column :content_types, :hide_imageuploads
   end
 end
